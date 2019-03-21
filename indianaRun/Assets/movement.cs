27fdb420ba73b3922136a67922d7c0259cc26942
badwulf51 <aronomalley@yahoo.com>
2019-03-21 10:15:53 +0000

    added the completed project to github

---
+++ indianaRun/Assets/movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement : MonoBehaviour {
+	// movement variables 
+	public float moveSpeed = 300;
+	public float jumpForce = 800f;
+	public GameObject character;
+	private Rigidbody2D characterBody; 
+
+	private float ScreenWidth;
+	private bool jump = false; 
+
+	// Use this for initialization
+	void Start () {
+		ScreenWidth = Screen.width; 
+		characterBody = character.GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int i=0;
+		// Loops over each touch 
+		while (i < Input.touchCount){
+			if(Input.GetTouch (i).position.x > ScreenWidth / 2){
+				RunCharacter (1.0f);
+		}
+			if(Input.GetTouch (i).position.x < ScreenWidth / 4){
+				RunCharacter (-1.0f);
+		}
+		++i;
+	}
+}
+// used for testing touch controls inside unity editor 
+void FixedUpdate(){
+#if UNITY_EDITOR
+RunCharacter(Input.GetAxis("Horizontal"));
+#endif
+
+}
+
+	private void RunCharacter(float horizontalInput){
+		characterBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
+	}
+
+	//private void jumpCharacter(float verticalInput){
+		//characterBody.AddForce(new Vector2(verticalInput * jumpForce * Time.deltaTime, 0));
+	//}
+
+}
---
+++ indianaRun/Temp/gradleOut/build/generated/source/buildConfig/release/ie/gmit/sw/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ie.gmit.sw;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ie.gmit.sw";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
